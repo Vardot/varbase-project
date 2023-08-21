f1a9983be1e77dec4e9ee94740626a3f1939d97f
Rajab Natshah <rajabn@gmail.com>
2023-08-21 20:34:58 +0300

    Add Autodoc config for Storybook Docs in Varbase Storybook to work with mdx files in components #187

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,16 +3,20 @@ const config = {
   // Change the place where storybook searched for stories.
   stories: [
     // Listing Varbase Components in the Storybook
+    "../docroot/modules/contrib/varbase_components/components/**/*.mdx",
     "../docroot/modules/contrib/varbase_components/components/**/*.stories.@(json|yml)",
     // -------------------------------------------------------------------------------
     // Uncomment the following line to start listing components Vartheme BS5 Starterkit.
+    // "../docroot/themes/contrib/vartheme_ba5/components/**/*.mdx",
     // "../docroot/themes/contrib/vartheme_ba5/components/**/*.stories.@(json|yml)",
     // -------------------------------------------------------------------------------
     // Uncomment the following line to start listing components from custom cloned generated theme
-    // Change `mytheme` to the name of the custom theme.  
+    // Change `mytheme` to the name of the custom theme.
+    // "../docroot/themes/custom/mytheme/components/**/*.mdx",
     // "../docroot/themes/custom/mytheme/components/**/*.stories.@(json|yml)",
     // -------------------------------------------------------------------------------
     // Uncomment the following line to start listing components from custom modules
+    // "../docroot/modules/custom/my_custom_module/components/**/*.mdx",
     // "../docroot/modules/custom/my_custom_module/components/**/*.stories.@(json|yml)",
   ],
   addons: [
@@ -22,6 +26,7 @@ const config = {
     '@storybook/manager-api',
     '@storybook/theming',
     '@storybook/addon-a11y',
+    '@storybook/blocks',
     'storybook-addon-root-attribute/register',
     'storybook-addon-rtl-direction'
   ],
