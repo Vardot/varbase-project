1efb8a5b33f27f3388aa4217c9633d2b08cda2fa
Rajab Natshah <rajabn@gmail.com>
2026-04-06 23:44:13 +0300

    feat: #3566900 Change order and groups of stories in the Storybook to match with Drupal Canvas and Atomic Design

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,12 +4,108 @@ const preview: Preview = {
   parameters: {
     options: {
       storySort: {
-        order: ['Getting Started', ['Welcome', 'About Varbase'], 'Foundation', ['Logos', 'Colors', 'Typography', 'Spacing & Grids', 'Interaction Guideline', 'Icons', 'Images'], 'Blocks', ['*'], 'Components', ['*'], 'Atoms', ['*'], 'Molecules', ['*'], 'Organisms', ['*'], 'Templates', ['*'], 'Pages', ['*']],
+        order: [
+          'Getting Started', ['Welcome', 'About Varbase'],
+          'Foundation', [
+            'Colors',
+            'Icons',
+            'Images',
+            'Interaction Guideline',
+            'Logos',
+            'Spacing & Grids',
+            'Typography',
+            '*'
+          ],
+          'Base', [
+            'Anchor',
+            'Badge',
+            'Blockquote',
+            'Heading',
+            'HTML Code',
+            'Icon',
+            'Plain Text',
+            'Rich Text',
+            'Taxonomy',
+            'Text',
+            '*'
+          ],
+          'Hero', [
+            'Hero Billboard',
+            'Hero Blog',
+            'Hero Card',
+            'Hero Heroslider',
+            'Hero Side by Side',
+            'Hero Slider Container',
+            '*'
+          ],
+          'Layout', [
+            'Column',
+            'Group',
+            'Horizontal Ruler',
+            'Row',
+            'Section',
+            'Spacer',
+            '*'
+          ],
+          'Cards', [
+            'Card',
+            'Card Icon',
+            'Card Logo',
+            'Card Overlay',
+            'Featured Card',
+            'Hero Card',
+            'Impressed Card',
+            'Pricing card',
+            'Testimonial Card',
+            'Text Card',
+            '*'
+          ],
+          'Atoms', [
+            'Button',
+            'Close Button',
+            'Image',
+            'Input',
+            'Input Checkbox',
+            'Input Submit',
+            'Link',
+            'List',
+            'Page Title',
+            'Progress Bar',
+            'Spinner',
+            'Textarea',
+            '*'
+          ],
+          'Molecules', [
+            'Accordion',
+            'Alert',
+            'Breadcrumb',
+            'Callout',
+            'Field tags',
+            'Grid',
+            'List Group',
+            'Pagination',
+            'Table',
+            '*'
+          ],
+          'Organisms', [
+            'Carousel',
+            'CTA',
+            'Heroslider',
+            'Media Header',
+            'Navbar',
+            'View Grid',
+            'View Heroslider',
+            'View List',
+            'View Summary',
+            'View Table',
+            '*'
+          ],
+        ],
       }
     },
     server: {
       // Replace this with your Drupal site URL, or an environment variable.
-      url: process.env.STORYBOOK_SERVER_RENDER_URL,
+      url: getServerUrl(),
     },
     controls: {
       matchers: {
